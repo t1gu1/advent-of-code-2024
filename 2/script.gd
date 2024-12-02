f7dfec5fdcd9a74b72be21d36ccf2e26855b767e
Ghuard <guillaumehuardhughes@gmail.com>
2024-12-02 03:02:20 -0500

    commit 2

---
+++ 2/script.gd
@@ -0,0 +1,14 @@
+# COmmand to run the script fast and easy
+# godot --headless -s --quit
+
+extends Node
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+    # Called every time the node is added to the scene.
+    # Initialization here
+    print_debug('hello 2')
+    pass
