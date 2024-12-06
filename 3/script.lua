e736987e6b815eab74e82a7f2eeb867e63fdd250
Guillaume Huard Hughes <guillaumehuardhughes@gmail.com>
2024-12-06 00:40:03 -0500

    Clean up day 3 part 1

--- 3/script.lua
+++ 3/script.lua
@@ -1,28 +1,24 @@
 local input = io.open("input.txt", "r")
-local countIteration = 0
-local additionnedMultiplication = 0
 
-function PrintOnlyFiveFirstLines(toPrint)
-	if countIteration < 5 then
-		print(toPrint)
-	end
-end
-
+-- Should only count the mul(X,Y) instructions where X and Y are numbers.
+-- No space or other caracters should be present in the instruction.
 function Part1(line)
-	-- Should only count the mul(X,Y) instructions where X and Y are numbers.
-	-- No space or other caracters should be present in the instruction.
+	local additionnedMultiplication = 0
 	for a, b in line:gmatch("mul%((%d+),(%d+)%)") do
 		additionnedMultiplication = additionnedMultiplication + a * b
-		countIteration = countIteration + 1
 	end
+	return additionnedMultiplication
 end
+
+function Part2(line) end
 
 -- WHERE IT START
 if input then
+	local accLines = ""
 	for line in input:lines() do
-		Part1(line)
+		accLines = accLines .. line
 	end
 	input:close()
 
-	print("additionnedMultiplication: " .. additionnedMultiplication)
+	print("additionnedMultiplication: " .. Part1(accLines))
 end
